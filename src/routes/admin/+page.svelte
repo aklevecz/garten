28a41e444c3059ac6fc66e0696c7628787cef747
aklevecz <teh@raptor.pizza>
2023-07-18 17:10:22 -0700

    admino

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import { enhance } from "$app/forms";
   import GoogleMaps from "$components/GoogleMaps.svelte";
+  import Egg from "$components/svg/Egg.svelte";
   import Locate from "$components/svg/Locate.svelte";
   import mapUtils from "$lib/mapUtils";
+  import { redirect } from "@sveltejs/kit";
   import type { PageData } from "./$types";
 
   export let data: PageData;
@@ -11,13 +13,20 @@
   let mapContainer: HTMLDivElement;
   let position = { lat: 0, lng: 0 };
 
+  let canvasRef: HTMLCanvasElement;
+  $: {
+    if (map && data.markers) {
+      for (const marker of data.markers) {
+        mapUtils.createMarker(map, marker.position, marker.name, mapUtils.eggIcon());
+      }
+    }
+  }
+
   function onLoad(e: CustomEvent) {
     map = e.detail.map as google.maps.Map;
-
-    for (const marker of data.markers) {
-      mapUtils.createMarker(map, marker.position, marker.name, mapUtils.eggIcon());
-    }
-
+    // for (const marker of data.markers) {
+    //   mapUtils.createMarker(map, marker.position, marker.name, mapUtils.eggIcon());
+    // }
     // new google.maps.Marker({ position: map.getCenter(), map, draggable: true });
     const input = document.createElement("input") as HTMLInputElement;
     input.style.width = "300px";
@@ -57,12 +66,52 @@
     });
     let lat = center!.lat();
     let lng = center!.lng();
+    position = { lat, lng };
     // @ts-ignore
     placeMarker.addListener("drag", (e: any) => {
       lat = e.latLng.lat();
       lng = e.latLng.lng();
       position = { lat, lng };
     });
+  };
+
+  const onQR = async (qrString: string) => {
+    // @ts-ignore
+    const qr = (await import("qrcode")).default;
+    let eggImg = new Image();
+    const canvasDims = { width: 1275, height: 1650 };
+    const ctx = canvasRef.getContext("2d")!;
+    eggImg.onload = () => {
+      qr.toDataURL(qrString, {
+        width: 1080,
+        type: "terminal",
+        color: {
+          light: "#4C4D4D",
+          dark: "#FFF",
+        },
+      }).then((url: string) => {
+        let img = new Image();
+        img.src = url;
+
+        const imgDim = 1200;
+        ctx.drawImage(
+          eggImg,
+          // canvasDims.width * 0.5 - imgDim / 2,
+          // canvasDims.height * 0.5 - imgDim / 2,
+          0,
+          0,
+          canvasDims.width,
+          canvasDims.height
+        );
+
+        img.onload = () => {
+          const dim = 150;
+
+          ctx?.drawImage(img, canvasDims.width * 0.5 - dim * 0.5 + 200, canvasDims.height - 500, dim, dim);
+        };
+      });
+    };
+    eggImg.src = "http://localhost:5173/egg-qr-template.png";
   };
 </script>
 
@@ -73,6 +122,8 @@
       <div class="marker-wrapper">
         <div>{marker.name}</div>
         <div>{marker.hunt}</div>
+        <div>{marker.code}</div>
+        <button on:click={() => onQR(`https://garten-six.vercel.app/find/${marker.code}`)}>qr</button>
       </div>
     {/each}
   </div>
@@ -82,8 +133,9 @@
     <input placeholder="code" name="code" />
     <input placeholder="lat" name="lat" bind:value={position.lat} />
     <input placeholder="lng" name="lng" bind:value={position.lng} />
-    <button>submit</button>
+    <div><button>create</button></div>
   </form>
+  <canvas bind:this={canvasRef} width="1275" height="1650" />
   <div class="map-container" bind:this={mapContainer}>
     <GoogleMaps height="100%" on:load={onLoad} />
   </div>
@@ -103,16 +155,18 @@
 <button class="user-position" style="bottom:100px;" on:click={onMarkClick}>MARK</button>
 
 <style>
+  canvas {
+    width: 100px;
+  }
   .container {
     display: flex;
     flex-direction: column;
     height: 100%;
     padding: 8px;
   }
-  .marker-container {
-    display: flex;
-  }
   .marker-wrapper {
+    display: flex;
+    gap: 8px;
     border: 1px solid black;
     margin: 10px;
     padding: 4px;
@@ -120,7 +174,11 @@
   form {
     display: flex;
     flex-direction: column;
+    gap: 8px;
     width: 50%;
+  }
+  form > input {
+    width: 200px;
   }
   .map-container {
     flex: 1 0 auto;
